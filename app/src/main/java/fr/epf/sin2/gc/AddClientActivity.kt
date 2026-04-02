74791c6005bcda09d95a1752a567d6d195abb4d3
Cyril Dumont <cyril.dumont@ouidou.fr>
2026-04-02 17:03:49 +0200

    Fin du cours du 02/04/2026

--- app/src/main/java/fr/epf/sin2/gc/AddClientActivity.kt
+++ app/src/main/java/fr/epf/sin2/gc/AddClientActivity.kt
@@ -5,6 +5,10 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "AddClientActivity"
@@ -18,8 +22,39 @@ class AddClientActivity : AppCompatActivity() {
 
         val addButton = findViewById<Button>(R.id.add_client_button)
 
+        val genderGroup = findViewById<RadioGroup>(R.id.add_client_gender_radiogroup)
+
+        val ageTextview = findViewById<TextView>(R.id.add_client_age_textview)
+        val ageSeekbar = findViewById<SeekBar>(R.id.add_client_age_seekbar)
+
+      
+        
+        ageSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(
+                p0: SeekBar?,
+                progress: Int,
+                p2: Boolean
+            ) {
+                ageTextview.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) = Unit
+            override fun onStopTrackingTouch(p0: SeekBar?) = Unit
+
+        });
+        
         addButton.setOnClickListener{
             Log.d(TAG, "Nom: ${lastnameEdittext.text}")
+
+            val gender =
+                if(genderGroup.checkedRadioButtonId == R.id.add_client_gender_man_radiobutton)
+                    "Homme" else "Femme"
+
+            Log.d(TAG, "Sexe : ${gender}")
+            Log.d(TAG, "Age: ${ageSeekbar.progress}")
+
+            Toast.makeText(this, "Bravo !!", Toast.LENGTH_SHORT).show()
+
         }
     }
 }
